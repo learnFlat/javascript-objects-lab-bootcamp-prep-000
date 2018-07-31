0c7c7f760935bde6d0d02be8adf81f286120f3a8
C Z <xzcynthiazheng@gmail.com>
2018-07-31 19:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectByKey() {
+function deleteFromObjectByKey(bject, key) {
   
 } 
